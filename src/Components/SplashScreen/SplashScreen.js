9b27ce4baa86fa36c1094c54b8dce3afd828eb9f
noelloera <loera.noel@gmail.com>
2020-08-15 17:51:20 -0700

    Splash screen for application on load method added

--- src/Components/SplashScreen/SplashScreen.js
+++ src/Components/SplashScreen/SplashScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import "./SplashScreen.css"
 
 export default class SplashScreen extends React.Component{
+    displayOnLoad(){
+        
+    }
     render(){
         return(
             <div className="Splash-screen">
-                <img
+                <img></img>
             </div>
         )
     }
