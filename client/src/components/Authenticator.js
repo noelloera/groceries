e90b1d0d325acfad7e568e02aec5d59226084fbb
Noel Loera <50557498+noelloera@users.noreply.github.com>
2020-10-22 20:52:57 -0700

    Updated Authenticator.js for token management
    Added token management upon Authenticator component mounting which will check the access and refresh token and react according pass or fail statuses.

--- client/src/components/Authenticator.js
+++ client/src/components/Authenticator.js
@@ -6,17 +6,43 @@ import {
   clearAccess,
   clearRefresh,
 } from "../helpers/jwt.js";
+import axios from "axios";
+
 export default class Authenticator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       authenticated: false,
     };
+      this.newAccess = this.newAccess.bind(this)
+  }
+
+  newAccess(refresh) {
+    if (refresh) {
+      axios
+        .post("/token", { token: refresh })
+        .then((res) => {
+          console.log(res.data.access);
+          localStorage.setItem("access", res.data.access);
+          this.props.history.push("/lists");
+        })
+        .catch((err) => {
+          console.log(err);
+          clearRefresh();
+          this.props.history.push("/login");
+        });
+    }
   }
   //Work on the authenticated user mounting and routing accordingly
-  componentDidMount() {
-    if (!getAccess()) {
-      this.props.history.push("/login");
+  async componentDidMount() {
+    const access = getAccess();
+    const refresh = getRefresh();
+    if (!access && refresh) {
+      await this.newAccess()
     }
+      if (access) {
+        axios.get("/me", {headers: {Authorization: `Bearer ${access}`}})
+    }
+     if() 
   }
 }
